1176cf13019b99d6c8793e7718ab2fb767e7dbde
aline marinho <linee_marinho@outlook.com>
2024-02-09 17:40:37 -0300

    ajustes

--- src/app/pages/home/patients/components/sheets/sheets.component.ts
+++ src/app/pages/home/patients/components/sheets/sheets.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-sheets',
-  templateUrl: './sheets.component.html',
-  styleUrls: ['./sheets.component.scss']
+  selector: "app-sheets",
+  templateUrl: "./sheets.component.html",
+  styleUrls: ["./sheets.component.scss"],
 })
 export class SheetsComponent {
-
+  detail = false;
+  openItem() {
+    this.detail = !this.detail;
+  }
 }
