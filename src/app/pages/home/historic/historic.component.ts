4b987fe6639a04436a7d3251b1553e17ff597e1e
aline marinho <linee_marinho@outlook.com>
2024-02-08 22:10:06 -0300

    itens

---
+++ src/app/pages/home/historic/historic.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-historic",
+  templateUrl: "./historic.component.html",
+  styleUrls: ["./historic.component.scss"],
+})
+export class HistoricComponent {
+  iconAdd = "assets/images/icons/iconAdd.png";
+  iconDate = "assets/images/icons/iconDate.png";
+  back() {}
+  close() {}
+}
